80809599ba4d15179fde2db4552bec68c855f7b6
hossammohammed72 <hossammohammed72@gmail.com>
2017-03-29 01:24:39 +0200

    Update powerFunction.cpp

--- powerFunction.cpp
+++ powerFunction.cpp
@@ -3,7 +3,6 @@ using namespace std ;
 int power(int base , int exponent){
 if(exponent==1)
 return base ; 
-cout << exponent << endl ; 
 if(exponent%2 ==0 ){
 return power(base,exponent/2)*power(base,exponent/2);}
 else {
