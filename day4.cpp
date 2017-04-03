3d7bf4ce72aa0edadb3450e92b11e2a2215995ec
hossammohammed72 <hossammohammed72@gmail.com>
2017-04-03 02:15:33 +0200

    trying 792/B codeforces (still not solved)

---
+++ day4.cpp
@@ -0,0 +1,23 @@
+//http://codeforces.com/problemset/problem/792/B
+#include <iostream>
+using namespace std ; 
+int main(){
+int n,k ; 
+cin >> n >> k ; 
+int a[k],o[k]; 
+int leader = 1 ;
+for(int i=0;i<k;i++){
+cin>>a[i]; 
+while( a[i] >1 ){ 
+if(leader==n){
+leader = 1 ;
+}
+leader++;
+a[i]-- ; 
+}
+o[i] = leader++;  
+n--;}
+for(int i=0;i<k;i++) 
+cout << o[i] << " ";
+return 0 ; 
+}
