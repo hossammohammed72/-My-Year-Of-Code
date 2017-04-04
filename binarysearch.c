f2e42830d6b0e356130c4b6a49800b8d86b068dd
hossammohammed72 <hossammohammed72@gmail.com>
2017-04-04 02:41:46 +0200

    Day6

---
+++ binarysearch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//Day 6 binary search 
+void  sort(int *arr,int n){
+	for(int i= n-1;i>0;i--){
+		for(int j=0;j<i;j++)
+			if(arr[j] > arr[j+1]){
+				int temp = arr[j+1];	
+				arr[j+1] = arr[j];
+				arr[j]=temp; 
+			}
+	}	
+}
+int main(){
+int n ; 
+scanf("%d",&n);
+int arr[n] ; 
+for(int i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+sort(arr,n) ; 
+for(int i=0;i<n;i++)
+	printf("%d",arr[i]);
+
+return 0 ; 
+}
